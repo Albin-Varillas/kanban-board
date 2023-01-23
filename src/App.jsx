adc34b65e17d18afd43823642472934d31c051ad
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-23 16:03:02 +0100

    added board and tickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Board from "./components/Board/Board"
 import SideNav from "./components/SideNav/SideNav"
 
 export default function App() {
     return (
         <div className="App">
             <SideNav />
+            <Board />
         </div>
     )
 }
