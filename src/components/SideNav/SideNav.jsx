0459ea7a4256d24680dd5e6195d98b5c7b657305
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-23 11:17:44 +0100

    added sidebar

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -5,6 +5,12 @@ export default function SideNav() {
     return (
         <div className="sidenav">
             <p>ALL BOARDS (2)</p>
+            <ul>
+                <li>Kanvan board</li>
+                <li>Kanvan board</li>
+                <li className="active">Kanvan board</li>
+                <li>Kanvan board</li>
+            </ul>
         </div>
     )
 }
