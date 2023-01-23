2188f73e2c863e9df23fb412f4687ddd45953dc5
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-23 17:00:02 +0100

    started on projectcontext

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,15 +1,19 @@
 import React from "react"
 import "./SideNav.scss"
+import { useProject } from "../../context/ProjectContext"
 
 export default function SideNav() {
+    const { projects } = useProject()
+    console.log(projects)
+
     return (
         <div className="sidenav">
             <p>ALL BOARDS (2)</p>
+
             <ul>
-                <li>Kanvan board</li>
-                <li>Kanvan board</li>
-                <li className="active">Kanvan board</li>
-                <li>Kanvan board</li>
+                {projects.map((project) => (
+                    <li key={project.id}>{project.title}</li>
+                ))}
             </ul>
         </div>
     )
