4df6cdf477f1f904a6929101529d06fbdec882fc
Albin Varillas <81118732+Albin-Varillas@users.noreply.github.com>
2023-01-26 16:37:38 +0100

    added dnd funtionality

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import ProjectProvider from "./context/ProjectContext"
 import "./index.scss"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ProjectProvider>
-            <App />
-        </ProjectProvider>
-    </React.StrictMode>
+    <ProjectProvider>
+        <App />
+    </ProjectProvider>
 )
